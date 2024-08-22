0b0feb938653e079d15391750943bdef53da9550
phani-rec <phaneendra.allu@raghuenggcollege.in>
2024-08-22 14:54:59 +0530

    boolean datatype added

---
+++ datatypes/Boolean01.java
@@ -0,0 +1,18 @@
+//boolean -->  byte -->   char -->   short -->   int -->    long -->   float -->    double 
+class Boolean01
+{
+  static boolean b0;
+  public static void main(String[] args)
+  {
+    boolean b1=true;  
+    boolean b2=false;
+   // boolean b3=0;
+   // boolean b4=True;
+   // boolean b5=TRUE;
+    
+    System.out.println(b0);   //false as default value
+    System.out.println(b1);  
+    System.out.println(b2);  
+   
+   }
+}
