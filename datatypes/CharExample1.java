54712df335d59e92e6cde07a9faa15e0447b631f
phani-rec <phaneendra.allu@raghuenggcollege.in>
2024-08-28 10:47:38 +0530

    char example

---
+++ datatypes/CharExample1.java
@@ -0,0 +1,13 @@
+    public class CharExample1 
+	{
+    
+        public static void main(String[] args) {  
+      
+            char char1='a';  
+            char char2='A';  
+              
+      
+            System.out.println("char1: "+char1);  
+            System.out.println("char2: "+char2);                  
+        }         
+    }  
---
+++ datatypes/CharExample2.java
@@ -0,0 +1,12 @@
+    public class CharExample2 {  
+      
+        public static void main(String[] args) {  
+      
+            char char1=65;  
+            char char2=97;  
+              
+            System.out.println("char1: "+char1);  
+            System.out.println("char2: "+char2);  
+      
+        }  
+    }  
