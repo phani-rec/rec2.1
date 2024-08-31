f11dd9cbb1d275920f567063b1f132005c116ead
phani-rec <phaneendra.allu@raghuenggcollege.in>
2024-08-31 11:08:50 +0530

    Float added

---
+++ datatypes/FloatExample1.java
@@ -0,0 +1,10 @@
+    public class FloatExample1 {  
+      
+        public static void main(String[] args) {  
+              
+            float num1=5.5f;  
+            float num2=5f;  
+            System.out.println("num1: "+num1);  
+            System.out.println("num2: "+num2);  
+        }     
+    }  
---
+++ datatypes/FloatExample2.java
@@ -0,0 +1,12 @@
+    public class FloatExample2 {  
+      
+        public static void main(String[] args) {  
+      
+            float num1=581216732.323433f;  
+            float num2=7.83684987683688f;  
+            System.out.println("num1: "+num1);  
+            System.out.println("num2: "+num2);  
+      
+        }  
+          
+    }  
