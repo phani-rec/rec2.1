3b596e62a28f624eff7f4aab165cdd8fa10cdcc1
phani-rec <phaneendra.allu@raghuenggcollege.in>
2024-09-15 15:05:44 +0530

    change dir name

--- Demo.java
+++ Demo.java
@@ -4,7 +4,8 @@ public static void main(int... args)
  {
   System.out.println("Hello world  int");
  }
-public static void main(String[] args)
+//static synchronized strictfp final public void main(String[] args)  //way 1
+public static void main(String[] args)  // way2 
  {
   System.out.println("Hello world String ");   
    new Demo().main();
