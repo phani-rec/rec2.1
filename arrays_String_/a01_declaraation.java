14ee7c49d7749654b8e17a2c82b20330fd7b5af2
phani-rec <phaneendra.allu@raghuenggcollege.in>
2024-09-24 10:03:39 +0530

    arrays dir added

---
+++ arrays_String_/a01_declaraation.java
@@ -0,0 +1,15 @@
+class Declaration
+{
+  public static void main(String[] args)
+  {
+	  System.out.println("array declaration ......");
+	  byte[] b1;
+	  short[] s1;
+	  int[] a;
+	  char[] ch;
+	  long[] l1;
+	  float[] f;
+	  double[] d;	
+		//System.out.println(b1.getClass().getName());
+  }
+}
