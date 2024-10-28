67abebe8de54fb1162dbde05265c7db11324ebd6
phani-rec <phaneendra.allu@raghuenggcollege.in>
2024-10-28 11:14:32 +0530

    import package.*

---
+++ inheritance_packages_exception/p02_A.java
@@ -0,0 +1,20 @@
+package rec;  
+class A
+{
+	public void msg()
+	{
+		System.out.println("A class from rec packages msg method");
+	}
+}  
+
+/*
+
+How to access package from another package?
+
+There are three ways to access the package from outside the package.
+
+1 import package.*;
+2 import package.classname;
+3 fully qualified name.
+
+*/
---
+++ inheritance_packages_exception/p03_B.java
@@ -0,0 +1,11 @@
+package rit ;  
+import rec.*;  
+  
+class B
+{
+	public static void main(String args[])
+	{
+		A obj = new A();
+		obj.msg();
+	}  
+}  
