ca803b64f94e90a65bbe3f61929a71633093a25a
phani-rec <phaneendra.allu@raghuenggcollege.in>
2024-10-28 10:57:27 +0530

    packages example added

---
+++ inheritance_packages_exception/p01_create.java
@@ -0,0 +1,12 @@
+package raghu;  
+class Demo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to package");
+		System.out.println("package raghu creation");
+		System.out.println("complitation  process : javac -d directory javafilename  ");
+		System.out.println("complitation  process : javac -d . p01_create.java  ");
+		System.out.println("run  process : java raghu.Demo  ");
+	}  
+}  
